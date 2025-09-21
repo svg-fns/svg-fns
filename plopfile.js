84be19a71b6338a657315f34f55fbe30c4f9eac4
Mayank <mayank.srmu@gmail.com>
2025-09-21 11:10:47 +0530

    Add plop generator for generating new packages

---
+++ plopfile.js
@@ -0,0 +1,24 @@
+import path from "node:path";
+
+export default function (plop) {
+  plop.setGenerator("package", {
+    description: "Scaffold a new @svg-fns package",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Package name (e.g. transform):",
+        validate: (input) => (input ? true : "Package name is required"),
+        filter: (input) => input.trim(),
+      },
+    ],
+    actions: [
+      {
+        type: "addMany",
+        destination: "packages",
+        base: "scripts/plop-templates",
+        templateFiles: "scripts/plop-templates/**",
+      },
+    ],
+  });
+}
