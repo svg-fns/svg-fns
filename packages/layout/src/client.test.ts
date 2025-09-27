61bb4d581e2e8a37a9dfd8d81e2d7fb9f21eb7aa
Mayank <mayank.srmu@gmail.com>
2025-09-27 14:13:03 +0530

    Scaffold layout

---
+++ packages/layout/src/client.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "vitest";
+import {} from "./client";
+
+describe("layout/client", () => {
+  it("smoke", async ({ expect }) => {
+    // "implement this test";
+  });
+});
---
+++ packages/layout/src/server.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "vitest";
+import {} from "./server";
+
+describe("layout/client", () => {
+  it("smoke", async ({ expect }) => {
+    // "implement this test";
+  });
+});
